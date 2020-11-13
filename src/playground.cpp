af2f5b11544c721eb0ad0887a98eb00cb4b8da5b
arem54 <arem5454@gmail.com>
2020-11-13 14:12:12 -0700

    timing

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <chrono>
 
-int main() 
+struct Timer
 {
-	std::cout << "Hello World" << std::endl;
+    std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::duration<float> duration;
+    
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+
+        float ms = duration.count() * 1000.0f;
+        std::cout << ms << "ms\n";
+    }
+};
+
+int main()
+{
+    Timer timer;
+    std::cout << "Hello, World\n";
 }
